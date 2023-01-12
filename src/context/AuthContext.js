c92064b0c47a1d62ba1efed5a86d76a2627b0781
aryaan02 <aryaan.khan@yale.edu>
2023-01-12 10:26:05 -0800

    Delete Card component and use redirect sign in

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,7 +1,6 @@
 import { useContext, createContext, useEffect, useState } from "react";
 import {
   GoogleAuthProvider,
-  signInWithPopup,
   signInWithRedirect,
   signOut,
   onAuthStateChanged,
@@ -14,7 +13,7 @@ export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider);
+    signInWithRedirect(auth, provider);
   };
 
   const logOut = () => {
