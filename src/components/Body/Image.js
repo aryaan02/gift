7fa03001b2d3b50c88941ce865fd54a94b21e033
aryaan02 <aryaan.khan@yale.edu>
2023-01-29 12:39:27 -0500

    Add final descriptions and remove card flips

--- src/components/Body/Image.js
+++ src/components/Body/Image.js
@@ -3,14 +3,7 @@ import classes from "./Image.module.css";
 const Image = (props) => {
   return (
     <div className={classes.card}>
-      <div className={classes.inner}>
-        <div className={classes.front}>
-          <img src={props.photo} alt="Attachment from Ethan Email"></img>
-        </div>
-        <div className={classes.back}>
-          <h1>Description</h1>
-        </div>
-      </div>
+      <img src={props.photo} alt="Attachment from Ethan Email"></img>
     </div>
   );
 };
